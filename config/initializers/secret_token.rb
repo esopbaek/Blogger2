2b5ffea84abf22ae6e9ed55067116137631c727f
Esop <esop@appacademy.io>
2014-05-25 12:11:59 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '056072aa5d572a2e7e256626615c57de52d784fa7066657f7c80a587ffed1253196a13301731e6cf7a115cf19c1637cce9f3ceaf90d622ada7b6178db3e5e7df'
